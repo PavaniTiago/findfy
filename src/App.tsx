dc094e150be562230f6f4a917ba0b59537a80503
aaaaaaaaaaaa <aaaaaaaaaaaaaaaa>
2023-08-22 15:10:23 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className='bg-slate-950 w-full h-screen flex items-center justify-center'>
+      <h1 className='text-6xl text-slate-50'>Bolsonaro</h1>
+    </div>
+  )
+}
+
+export default App
